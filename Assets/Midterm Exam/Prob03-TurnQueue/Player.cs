9ab000232a163482794af7f876668a69ef21f87f
EmiyaHana <warm310149@gmail.com>
2026-09-22 10:53:28 +0700

    Finish

--- Assets/Midterm Exam/Prob03-TurnQueue/Player.cs
+++ Assets/Midterm Exam/Prob03-TurnQueue/Player.cs
@@ -51,6 +51,12 @@ namespace MidtermExam.Prob03
         public bool SwapQueue(LinkedList<Player> turnQueue, Player targetPlayer, Player afterPlayer)
         {
             // TODO: ให้นักศึกษา Implement การจัดการสลับลำดับของ Node ใน LinkedList<Player>
+            //TurnQueue.RemoveFirst();
+            //TurnQueue.AddLast(current);
+            turnQueue.Find(targetPlayer);
+            turnQueue.Remove(targetPlayer);
+            turnQueue.Contains(targetPlayer);
+            turnQueue.AddFirst(afterPlayer);
             return false;
         }
 
